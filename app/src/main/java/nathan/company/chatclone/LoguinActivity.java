4e3e340cc05df705dc28bb4ed179b81ed8a3fb2b
uenisReis <uenisprogramador@gmail.com>
2021-10-24 12:53:49 -0300

    criação da tela de loguin

---
+++ app/src/main/java/nathan/company/chatclone/LoguinActivity.java
@@ -0,0 +1,14 @@
+package nathan.company.chatclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoguinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loguin);
+    }
+}
