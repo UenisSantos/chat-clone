9acc5eff6c8b9d255c7b5906181231725735a792
uenisReis <uenisprogramador@gmail.com>
2021-10-24 18:37:37 -0300

    criação da tela cadastro de usuario

--- app/src/main/java/nathan/company/chatclone/LoguinActivity.java
+++ app/src/main/java/nathan/company/chatclone/LoguinActivity.java
@@ -2,7 +2,11 @@ package nathan.company.chatclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import nathan.company.chatclone.databinding.ActivityCadastoBinding;
 
 public class LoguinActivity extends AppCompatActivity {
 
@@ -11,4 +15,11 @@ public class LoguinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loguin);
     }
+
+    public void criaConta(View view){
+
+        Intent intent = new Intent(LoguinActivity.this, cadastoActivity.class);
+startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/nathan/company/chatclone/cadastoActivity.java
@@ -0,0 +1,14 @@
+package nathan.company.chatclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cadastoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadasto);
+    }
+}
